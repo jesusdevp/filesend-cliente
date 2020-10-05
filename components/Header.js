c3b76be031be0a64a593d36e382b3cb126f2f8ca
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-04 23:02:09 -0500

    Routing hacia Login y Crear Cuenta

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-      <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+      <Link href="/">
+        <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+      </Link>
+      <div>
+        <Link href="/login">
+          <a className="bg-red-600 px-5 py-3 rounded-lg text-white font-bold uppercase mr-3">
+            Iniciar Sesión
+          </a>
+        </Link>
+        <Link href="/crear-cuenta">
+          <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">
+            Crear Cuenta
+          </a>
+        </Link>
+      </div>
     </header>
   );
 };
---
+++ pages/crear-cuenta.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const CrearCuenta = () => {
+  return (
+    <Layout>
+      <h1>Crear Cuenta</h1>
+    </Layout>
+  );
+};
+
+export default CrearCuenta;
