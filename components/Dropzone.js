0f5217c1d7420174e577cef3f09e4ff7b9ba19a7
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-07 16:38:05 -0500

    Componente para dropzone

---
+++ components/Dropzone.js
@@ -0,0 +1,14 @@
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+import clieneAxios from "../config/axios";
+
+const Dropzone = () => {
+  return (
+    <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
+      <p>Dropzone aquí</p>
+    </div>
+  );
+};
+
+export default Dropzone;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import Layout from "../components/Layout";
+import Dropzone from "../components/Dropzone";
 import authContext from "../context/auth/authContext";
 import Link from "next/link";
 
@@ -16,9 +17,7 @@ const Index = () => {
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
         <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
-            <p>Dropzone aquí</p>
-          </div>
+          <Dropzone />
           <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
             <h2 className="text-4xl font-sans font-bold -text-gray-800 my-4">
               Compartir archivos de forma sencilla y privada
