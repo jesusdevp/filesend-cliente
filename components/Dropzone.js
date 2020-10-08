7d3f9afe6f88b5a24e74e4a8b33a488f9422a9e1
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-08 18:58:00 -0500

    Listando archivos para subir

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -6,7 +6,7 @@ import clienteAxios from "../config/axios";
 const Dropzone = () => {
   const onDrop = useCallback(async (acceptedFiles) => {
     // Crear un form Data
-    const formData = new formData();
+    const formData = new FormData();
     formData.append("archivo", acceptedFiles[0]);
 
     const resultado = await clienteAxios.post("/api/archivos", formData);
@@ -20,8 +20,22 @@ const Dropzone = () => {
     isDragActive,
     acceptedFiles,
   } = useDropzone({ onDrop });
+
+  const archivos = acceptedFiles.map((archivo) => (
+    <li
+      key={archivo.lastModified}
+      className="bg-white flex-1 p-3 mb-4 shadow-lg rounded"
+    >
+      <p className="font-bold text-xl">{archivo.path}</p>
+      <p className="text-sm text-gray-500">
+        {" "}
+        {(archivo.size / Math.pow(1024, 2)).toFixed(2)} MB{" "}
+      </p>
+    </li>
+  ));
   return (
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
+      <ul>{archivos}</ul>
       <div {...getRootProps({ className: "dropzone w-full py-32" })}>
         <input className="h-100" {...getInputProps()} />
         {isDragActive ? (
