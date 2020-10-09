1ece2c5f31dc7ed250d4c32eaa6e6bdd90a0dc8a
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-08 19:07:56 -0500

    Mostrando boton de crear enlace

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -33,29 +33,47 @@ const Dropzone = () => {
       </p>
     </li>
   ));
+
+  const crearEnlace = () => {
+    console.log("Creando el enlace");
+  };
+
   return (
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-      <ul>{archivos}</ul>
-      <div {...getRootProps({ className: "dropzone w-full py-32" })}>
-        <input className="h-100" {...getInputProps()} />
-        {isDragActive ? (
-          <p className="text-2xl text-center text-gray-600">
-            Suelta el archivo
-          </p>
-        ) : (
-          <div className="text-center">
+      {acceptedFiles.length > 0 ? (
+        <div className="mt-10 w-full">
+          <h4 className="text-2xl font-bold text-center mb-4">Archivos</h4>
+          <ul>{archivos}</ul>
+          <button
+            type="button"
+            className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+            onClick={() => crearEnlace()}
+          >
+            Crear Enlace
+          </button>
+        </div>
+      ) : (
+        <div {...getRootProps({ className: "dropzone w-full py-32" })}>
+          <input className="h-100" {...getInputProps()} />
+          {isDragActive ? (
             <p className="text-2xl text-center text-gray-600">
-              Selecciona un archivo y arrastralo aquí
+              Suelta el archivo
             </p>
-            <button
-              className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
-              type="button"
-            >
-              Selecciona archivos para subir
-            </button>
-          </div>
-        )}
-      </div>
+          ) : (
+            <div className="text-center">
+              <p className="text-2xl text-center text-gray-600">
+                Selecciona un archivo y arrastralo aquí
+              </p>
+              <button
+                className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+                type="button"
+              >
+                Selecciona archivos para subir
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
