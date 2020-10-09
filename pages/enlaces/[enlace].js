76a71044216bc50130816a1b3f218a85b2206e93
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-09 12:10:43 -0500

    Routing dinamico

---
+++ pages/enlaces/[enlace].js
@@ -0,0 +1,14 @@
+import Layout from "../../components/Layout";
+import clienteAxios from "../../config/axios";
+
+export async function getStaticProps() {}
+
+export async function getStaticPaths() {}
+
+export default () => {
+  return (
+    <Layout>
+      <h1>Desde [enlace].js</h1>
+    </Layout>
+  );
+};
