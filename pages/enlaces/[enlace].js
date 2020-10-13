33b68b15283822cf4ac0940adc0e910f978c53a6
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-13 14:17:51 -0500

    Funciones para generar URL

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -1,11 +1,27 @@
 import Layout from "../../components/Layout";
 import clienteAxios from "../../config/axios";
 
-export async function getStaticProps() {}
+export async function getStaticProps() {
+  const resultado = await clienteAxios.get("api/enlaces");
 
-export async function getStaticPaths() {}
+  return {
+    props: {
+      enlace: resultado.data,
+    },
+  };
+}
 
-export default () => {
+export async function getStaticPaths() {
+  const enlaces = await clienteAxios.get("/api/enlaces");
+  return {
+    paths: enlaces.data.enlaces.map((enlace) => ({
+      params: { enlace: enlace.url },
+    })),
+    fallback: false,
+  };
+}
+
+export default ({ enlace }) => {
   return (
     <Layout>
       <h1>Desde [enlace].js</h1>
